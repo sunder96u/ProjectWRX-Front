73ad8d6065af609f20f0c4d3ab48386cd40ee703
Steven Underwood <stevenunderwood2@hotmail.com>
2023-07-05 07:50:18 -0500

    Add layout to Task, TaskList, Login, and CreateAccount

--- ProjectNexus/src/components/Task.jsx
+++ ProjectNexus/src/components/Task.jsx
@@ -1,11 +1,68 @@
-
+import "../Task.css"
 
 
 
 export default function Task () {
-    return (
-        <h1>
-            Task Page
-        </h1>
-    )
+
+    const User = false
+
+    if (User) {
+        return (
+            <div className="background">
+                <div className="col">
+                    <div className="row">
+                        <p className="title">Task Name:</p>
+                        <p>(axios task name)</p>
+                    </div>
+                    <div className="row">
+                        <p className="title">Project Name:</p>
+                        <p>(axios project name here)</p>
+                    </div>
+                </div>
+                <div className="col">
+                    <p className="title"> Due Date:</p>
+                    <p className="date">(axios duedate)</p>
+                </div>
+                <div className="col">
+                    <p className="title">Description:</p>
+                    <p>(description here)</p>
+                </div>
+                <div className="col">
+                    <button>Completed</button>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+        <div className="background">
+            <div className="col">
+                <div className="row">
+                    <p className="title">Task Name:</p>
+                    <p>(axios task name)</p>
+                </div>
+                <div className="row">
+                    <p className="title">Project Name:</p>
+                    <p>(axios project name here)</p>
+                </div>
+            </div>
+            <div className="col">
+                <p className="title"> Due Date:</p>
+                <p className="date">(axios duedate)</p>
+            </div>
+            <div className="col">
+                <p className="title"> Submited By:</p>
+                <p>(axios call of who submited)</p>
+            </div>
+            <div className="col">
+                <p className="title">Description:</p>
+                <p>(description here)</p>
+            </div>
+            <div className="col">
+                <button>Reviewed & Completed</button>
+                <button>Reviewed & Rejected</button>
+            </div>
+        </div>
+        )
+    }
+
 }
